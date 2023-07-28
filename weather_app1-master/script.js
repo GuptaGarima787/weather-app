2c194426a1534b8c24c0780affd7195cff87a66c
Garima <gk7870931570@gmail.com>
2023-07-29 01:01:55 +0530

    Initial commit

---
+++ weather_app1-master/script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '51090aa4efmshaa08077779f259ep110d6bjsn7371fbb75f75',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
